afb2131dd5c028340528b873d4dcfc19ec4681c8
Marijn Haverbeke <marijnh@gmail.com>
2015-08-04 11:27:13 +0200

    Kludge to kill 'undefined users'

--- src/client/client.js
+++ src/client/client.js
@@ -178,6 +178,7 @@ document.querySelector("#changedoc").addEventListener("click", e => {
 })
 
 function userString(n) {
+  if (n == null) n = 1
   return "(" + n + " user" + (n == 1 ? "" : "s") + ")"
 }
 
