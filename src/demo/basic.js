d0eb5650ecaf0cb8f7064ad084f0f8ab392f2007
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-19 17:05:56 +0200

    Port the basic demo

--- src/demo/basic.js
+++ src/demo/basic.js
@@ -1,33 +1,22 @@
-const {ProseMirror} = require("prosemirror/dist/edit")
-const {schema} = require("prosemirror/dist/schema-basic")
-const {exampleSetup, buildMenuItems} = require("prosemirror/dist/example-setup")
-const {tooltipMenu, menuBar} = require("prosemirror/dist/menu")
+const {EditorState} = require("prosemirror-state")
+const {MenuBarEditorView} = require("prosemirror-menu")
+const {DOMParser, Schema} = require("prosemirror-model")
+const {schema: baseSchema} = require("prosemirror-schema-basic")
+const {addListNodes} = require("prosemirror-schema-list")
+const {exampleSetup} = require("prosemirror-example-setup")
 
-let place = document.querySelector("#editor")
-let content = document.querySelector("#content")
-
-let pm = window.pm = new ProseMirror({
-  place: place,
-  doc: schema.parseDOM(content),
-  plugins: [exampleSetup.config({menuBar: false, tooltipMenu: false})]
+const schema = new Schema({
+  nodes: addListNodes(baseSchema.nodeSpec, "paragraph block*", "block"),
+  marks: baseSchema.markSpec
 })
+
+let content = document.querySelector("#content")
 content.style.display = "none"
 
-let menu = buildMenuItems(schema)
-
-setMenuStyle(place.getAttribute("menustyle") || "bar")
-
-function setMenuStyle(type) {
-  if (type == "bar") {
-    tooltipMenu.detach(pm)
-    menuBar.config({float: true, content: menu.fullMenu}).attach(pm)
-  } else {
-    menuBar.detach(pm)
-    tooltipMenu.config({selectedBlockMenu: true,
-                        inlineContent: menu.inlineMenu,
-                        blockContent: menu.blockMenu}).attach(pm)
-  }
-}
-
-let menuStyle = document.querySelector("#menustyle")
-if (menuStyle) menuStyle.addEventListener("change", () => setMenuStyle(menuStyle.value))
+let view = new MenuBarEditorView(document.querySelector("#editor"), {
+  state: EditorState.create({
+    doc: DOMParser.fromSchema(schema).parse(content),
+    plugins: [exampleSetup({schema})]
+  }),
+  onAction: action => view.updateState(view.editor.state.applyAction(action))
+})
