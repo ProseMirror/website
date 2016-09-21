f887a8bcdbb9ecd3164d9e696aa57200c1f95fc9
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-21 10:01:42 +0200

    Port Markdown demo

--- src/demo/markdown.js
+++ src/demo/markdown.js
@@ -1,31 +1,28 @@
-const {ProseMirror} = require("prosemirror/dist/edit")
-const {elt} = require("prosemirror/dist/util/dom")
-const {defaultMarkdownParser, defaultMarkdownSerializer} = require("prosemirror/dist/markdown")
-const {schema} = require("prosemirror/dist/schema-basic")
-const {exampleSetup} = require("prosemirror/dist/example-setup")
+const {MenuBarEditorView} = require("prosemirror-menu")
+const {EditorState} = require("prosemirror-state")
+const {schema, defaultMarkdownParser, defaultMarkdownSerializer} = require("prosemirror-markdown")
+const {exampleSetup} = require("prosemirror-example-setup")
 
 let place = document.querySelector("#editor")
 
 let getContent
 function toTextArea(content, focus) {
-  let te = place.appendChild(elt("textarea", {style: "font-family: inherit; font-size: inherit"}))
+  let te = place.appendChild(document.createElement("textarea"))
+  te.style.cssText = "font-family: inherit; font-size: inherit"
   te.value = content
   if (focus !== false) te.focus()
   getContent = () => te.value
 }
 function toProseMirror(content) {
-  let pm = window.pm = new ProseMirror({
-    place: place,
-    doc: defaultMarkdownParser.parse(content),
-    plugins: [
-      exampleSetup.config({
-        menuBar: false,
-        tooltipMenu: true
-      })
-    ]
+  let view = new MenuBarEditorView(place, {
+    state: EditorState.create({
+      doc: defaultMarkdownParser.parse(content),
+      plugins: [exampleSetup({schema})]
+    }),
+    onAction: action => view.updateState(view.editor.state.applyAction(action))
   })
-  pm.focus()
-  getContent = () => defaultMarkdownSerializer.serialize(pm.doc)
+  view.editor.focus()
+  getContent = () => defaultMarkdownSerializer.serialize(view.editor.state.doc)
 }
 toTextArea(document.querySelector("#markdown_content").textContent, false)
 
