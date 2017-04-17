4ac4319640e2433bd95ee2154306625c981d31eb
Marijn Haverbeke <marijn@haverbeke.nl>
2017-04-17 21:38:59 +0200

    Fix header shifting to also shift closing tags
    Closes #55

--- src/build/templates.js
+++ src/build/templates.js
@@ -12,7 +12,7 @@ module.exports = function loadTemplates(config) {
   mold.defs.markdown = function(options) {
     if (typeof options == "string") options = {text: options}
     let text = markdown.render(config.markdownFilter ? config.markdownFilter(options.text) : options.text)
-    if (options.shiftHeadings) text = text.replace(/<h(\d)\b/ig, (_, d) => "<h" + (+d + options.shiftHeadings))
+    if (options.shiftHeadings) text = text.replace(/<(\/?)h(\d)\b/ig, (_, cl, d) => "<" + cl + "h" + (+d + options.shiftHeadings))
     return text
   }
   mold.defs.markdownFile = function(options) {
